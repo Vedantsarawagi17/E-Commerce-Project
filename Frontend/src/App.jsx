e4cd58f714471cbc78754ce7784cbd7c478e4ac7
Vedantsarawagi17 <vedantsarawagi17@gmail.com>
2025-12-24 01:35:47 +0530

    Capital Letter in "Home"

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from 'react-router-dom'
-import { Home } from './pages/home'
+import { Home } from './pages/Home'
 import { PlaceOrder } from './pages/PlaceOrder'
 import { Product } from './pages/Product'
 import { Orders } from './pages/Orders'
